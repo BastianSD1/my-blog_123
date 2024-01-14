22a7834728186b29da418e9f091b923bb5636671
BastianSD1 <139516022+BastianSD1@users.noreply.github.com>
2024-01-14 16:34:53 -0600

    Add files via upload

---
+++ app web filtro 114-116/main.js
@@ -0,0 +1,20 @@
+function speak(){
+    
+    setTimeout(function() { 
+        take_snapshot(); 
+        save();
+    }, 5000);
+}
+function preload(){
+    
+
+}
+
+function setup(){
+    
+
+}
+function Draw(){
+    
+
+}
